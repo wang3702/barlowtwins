ed17b645bab5b0c56cc2ef03aaa38d9301f373e8
Xiao Wang <wang3702@fb.com>
2021-08-11 13:02:33 -0400

    update on knn monitor for baseline

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ parser.add_argument('--lambd', default=0.0051, type=float, metavar='L',
                     help='weight on off-diagonal terms')
 parser.add_argument('--projector', default='8192-8192-8192', type=str,
                     metavar='MLP', help='projector MLP')
-parser.add_argument('--print-freq', default=100, type=int, metavar='N',
+parser.add_argument('--print-freq', default=10, type=int, metavar='N',
                     help='print frequency')
 parser.add_argument('--checkpoint-dir', default='./checkpoint/', type=Path,
                     metavar='DIR', help='path to checkpoint directory')
